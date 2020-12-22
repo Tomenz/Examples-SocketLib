c5f0a8e98c4fcb7c37cfbc074d7dbf233ed0ec8f
Thomas <Thomas@fam-hauck.de>
2020-12-22 13:19:05 +0100

    - fixed "Server Hello" Message in SSL connection - update of submodule

--- Ssl-Example.cpp
+++ Ssl-Example.cpp
@@ -64,8 +64,8 @@ void ServerThread(const bool* bStop)
     SslTcpServer sock;
 
     // 3 callback function to handle the server socket events
-    sock.BindErrorFunction([&](BaseSocket* pSock) { cout << "Server: server socket error" << endl; pSock->Close(); }); // Must call Close function
-    sock.BindCloseFunction([&](BaseSocket*) { cout << "Server: server socket closing" << endl; });
+    sock.BindErrorFunction([&](BaseSocket* pSock) { cout << "Server: server socket error" << endl << flush; pSock->Close(); }); // Must call Close function
+    sock.BindCloseFunction([&](BaseSocket*) { cout << "Server: server socket closing" << endl << flush; });
 
     sock.BindNewConnection([&](const vector<TcpSocket*>& lstSockets)
     {
@@ -77,7 +77,7 @@ void ServerThread(const bool* bStop)
                 // 3 callback functions to handle the sockets events
                 pSocket->BindFuncBytesReceived([&](TcpSocket* pSock)
                 {
-                    const size_t nAvalible = pSock->GetBytesAvailible();
+                    const size_t nAvalible = pSock->GetBytesAvailable();
 
                     if (nAvalible == 0) // Socket closed on remote
                     {
@@ -97,7 +97,7 @@ void ServerThread(const bool* bStop)
                         stringstream strOutput;
                         strOutput << pSock->GetClientAddr() << " - Server received: " << nRead << " Bytes, \"" << strRec << "\"" << endl;
 
-                        cout << strOutput.str();
+                        cout << strOutput.str() << flush;
 
                         strRec = "Server echo: " + strRec;
                         pSock->Write(&strRec[0], strRec.size());
@@ -109,14 +109,16 @@ void ServerThread(const bool* bStop)
                 {
                     // there was an error, we close the socket
                     pSock->Close();
-                    cout << "Server: accept socket error" << endl;
+                    cout << "Server: accept socket error" << endl << flush;
                 });
                 pSocket->BindCloseFunction([&](BaseSocket* pSock)
                 {
-                    cout << "Server: accept socket closing" << endl;
+                    cout << "Server: accept socket closing" << endl << flush;
                 });
 
                 pSocket->StartReceiving();  // start to receive data
+
+                pSocket->Write("Server Hello Message", 20);
             }
         }
     });
@@ -146,11 +148,11 @@ void ClientThread(const bool* bStop)
     bool bIsClosed = false;
 
     // client socket has 4 callback function
-    sock.BindErrorFunction([&](BaseSocket* pSock) { cout << "Client: socket error" << endl; pSock->Close(); }); // Must call Close function
-    sock.BindCloseFunction([&](BaseSocket*) { cout << "Client: socket closing" << endl; bIsClosed = true; });
+    sock.BindErrorFunction([&](BaseSocket* pSock) { cout << "Client: socket error" << endl << flush; pSock->Close(); }); // Must call Close function
+    sock.BindCloseFunction([&](BaseSocket*) { cout << "Client: socket closing" << endl << flush; bIsClosed = true; });
     sock.BindFuncBytesReceived([&](TcpSocket* pTcpSocket)
     {
-        size_t const nAvalible = pTcpSocket->GetBytesAvailible();
+        size_t const nAvalible = pTcpSocket->GetBytesAvailable();
 
         if (nAvalible == 0) // Socket closed on remote
         {
@@ -170,7 +172,7 @@ void ClientThread(const bool* bStop)
             stringstream strOutput;
             strOutput << pTcpSocket->GetClientAddr() << " - Client received: " << nRead << " Bytes, \"" << strRec << "\"" << endl;
 
-            cout << strOutput.str();
+            cout << strOutput.str() <<  flush;
         }
     });
 
@@ -187,6 +189,7 @@ void ClientThread(const bool* bStop)
     });
 
     // Bundle of root certificates can be downloaded from here
+    // https://curl.se/docs/caextract.html
     // https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
     sock.SetTrustedRootCertificates("certs/ca-root.crt");
     vector<string> Alpn({ { "h2" }, { "http/1.1" } });
@@ -194,7 +197,7 @@ void ClientThread(const bool* bStop)
 
     const bool bConnected = sock.Connect(strHost.c_str(), 3461, AF_INET); // force to use IPv4. if above ::1 is used on the server, use here AF_INET6
     if (bConnected == false)
-        cout << "error creating client socket" << endl;
+        cout << "error creating client socket" << endl << flush;
 
     while (*bStop == false)
     {
@@ -208,7 +211,7 @@ void ClientThread(const bool* bStop)
     {
         //sock.BindCloseFunction(static_cast<function<void(BaseSocket*)>>(nullptr));
         sock.Close();
-        // We wait until the callback above was called
+        // Alternativ we wait until the callback above was called
         while (bIsClosed == false)
             this_thread::sleep_for(chrono::milliseconds(10));
     }
